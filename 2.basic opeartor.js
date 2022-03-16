f384504dc4409b0f3635699e23d89a7450c609b3
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2022-03-16 23:39:52 +0530

    Add files via upload

---
+++ 2.basic opeartor.js
@@ -0,0 +1,13 @@
+const myName=`Priya`
+const mySurname='Mourya'
+console.log("my name is "+myName+" "+mySurname);//concatinating with operator
+let num=0;
+num++;//1
+console.log(num);//1
+num+=2;//3
+console.log(num);//3
+console.log(num**2);//3 raise to 2=3*3
+if(num>=10)console.log("false")
+else console.log("true")
+console.log(typeof num,typeof myName)
+//can refer to mdn documentation
