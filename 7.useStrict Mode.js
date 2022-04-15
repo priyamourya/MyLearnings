fed10406b773fb53d5c98a3fe8d99bd764046362
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2022-04-15 19:28:15 +0530

    Add files via upload

---
+++ 7.useStrict Mode.js
@@ -0,0 +1,5 @@
+"use strict";
+let canDrive = false;
+const age = 19;
+if (age === 19) caDrive = true; //this will give error if(using strict mode)...if not js will make new variable
+if (canDrive) console.log(canDrive);
