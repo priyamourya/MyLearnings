5f7d4733810cd82a2be03d492bf178bac713d065
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2022-04-18 17:14:51 +0530

    Add files via upload

---
+++ 8.function expression and decaration.js
@@ -0,0 +1,12 @@
+// function declaration
+function CalculateAge(birthYear){
+    return (2022-birthYear);
+}
+const age1=CalculateAge(1999);
+console.log(age1);
+//function declaration
+const cal2=function(birthYear){
+    return (2022-birthYear);
+}
+const age2=cal2(1998)
+console.log(age2);
