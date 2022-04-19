479a2425ec981d16c8671385c9e34e4bc41e3bcd
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2022-04-19 08:31:59 +0530

    Add files via upload

---
+++ 9.arrow function.js
@@ -0,0 +1,11 @@
+// shorter arrow function
+const fun=para=> 2022-para;
+const t=(fun(1999))
+console.log(t);
+
+//longer  arrow function
+const fun2=(obj1,obj2)=>{
+    return `${obj1} this is my birth year ${obj2}`
+}
+const hello=fun2("hellooo",90)
+console.log(hello);
